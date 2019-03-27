b22f95fe6e2489b7ffd7bfedb8ed805012efbed1
Pankaj chejara <panche@Pankajs-MacBook-Pro.local>
2019-03-27 17:48:05 +0200

    first commit

--- mca.py
+++ mca.py
@@ -6,12 +6,20 @@ from sklearn import preprocessing
 
 #data = pd.read_csv('datawithcat2.csv')
 
-data = pd.read_csv('ProcessedData_ProjectDay_20171122.csv')
+data_8nov = pd.read_csv('ProcessedData_ProjectDay_20171108.csv')
+data_18oct = pd.read_csv('ProcessedData_ProjectDay_20171018.csv')
+
+data_22nov = pd.read_csv('ProcessedData_ProjectDay_20171122.csv')
 #data1 = pd.read_csv('Data_16_original.csv')
-#data = pd.read_csv('8th16th.csv')
+ #data = pd.read_csv('8th16th.csv')
+data = data_8nov.append(data_18oct)
+
+
 
 # Deleting the first columns of sequence number
 data.drop(data.columns[0],axis=1,inplace=True)
+data_22nov.drop(data_22nov.columns[0],axis=1,inplace=True)
+
 
 # Printing basic info of dataset
 #print ('8th Nov dataset:',data.shape)
@@ -24,7 +32,10 @@ data.drop(data.columns[0],axis=1,inplace=True)
 #print(data['timestamp'])
 
 raw_data = data.drop(['timestamp','group'],axis=1)
-raw_data = data.fillna(value=0,method=None,axis=1)
+data_22nov.drop(['timestamp','group'],axis=1,inplace=True)
+
+data_22nov.fillna(data_22nov.mean(),inplace=True)
+#raw_data = data.fillna(value=0,method=None,axis=1)
 #raw_data1 = data1.drop(['timestamp','group'],axis=1)
 
 # Stadardizing the dataset
@@ -55,8 +66,11 @@ mfa = mfa.fit(raw_data)
 
 
 
-mfadf = mfa.row_coordinates(raw_data)
-mfadf.to_csv('mfa_ProjectDay_20171122_fillna.csv')
+mfadf = mfa.row_coordinates(data_22nov)
+#mfadf.to_csv('mfa_ProjectDay_18oct_8nov.csv')
+#data.to_csv('ProjectDay_18oct_8nov.csv')
+
+mfadf.to_csv('mfa_ProjectDay_22nov.csv')
 
 #mfadf1 = mfa1.row_coordinates(raw_data1)
 #mfadf1.to_csv('mfaresult_16th.csv')
